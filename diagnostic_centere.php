12552bc31d36b5f2283c48c493172cfefd92297e
rilindpozhegu <rilindp@gmail.com>
2017-07-02 14:45:19 +0200

    new updates

--- diagnostic_centere.php
+++ diagnostic_centere.php
@@ -43,34 +43,184 @@
 
 
 
-<!-- Staff Section -->
-    <?php include 'tabtest.php';?>
-
-
 <!-- Tab Section Diagnostic-->
 <section class="diagnostic_tabs">
     <div class="container">
         <div class="row">
              <!-- Nav tabs -->
-              <ul class="nav  nav-pills  nav-justified buttons_dia  wow fadeInUp" data-wow-duration="0.5s" data-wow-delay=".3s">
+             <div class="col-md-offset-2">
+              <ul class="nav nav-pills nav-fill buttons_dia  wow fadeInUp" data-wow-duration="0.5s" data-wow-delay=".3s">
 
-                    <li role="presentation" class="active button_dia_s"><a href="#home" aria-controls="home" role="tab" data-toggle="tab" class="buton_on_activ_diagno">Imaging And X-Ray</a></li>
-                    <li role="presentation" class="button_dia_s"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Laboratory</a></li>
+                    <li role="presentation" class="active button_dia_s">
+                        <a href="#home" aria-controls="home" role="tab" data-toggle="tab" class="buton_on_activ_diagno">Imaging And X-Ray</a>
+                    </li>
+                    <li role="presentation" class="button_dia_s">
+                        <a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Laboratory</a>
+                    </li>
               </ul>
+              </div>
               <!-- Tab panes -->
                 <div class="tab-content tab_cont_text_img_dia">
                     <div role="tabpanel" class="tab-pane active" id="home">
-                    <div class="col-md-3 no_padding">
-                        <img src="img/random/diagnostic-01.png" class="img-responsive">
+
+                    <section class="slider multiple-items">
+                        <div>
+                        <div class="col-md-3 no_padding">
+                            <img src="img/random/diagnostic-01.png" class="img-responsive">
+                        </div>
+                        <div class="col-md-9 let_ip">
+                            <h1>Innovation & Quality</h1>
+                            <p>We have a full imaging and x-ray department with fully computerised, automated digital processing. Because MediCorp Gulf strives for excellence in diagnosis, it also offers teleradiology and telereporting capabilities whereby a second, or even third, medical opinion from a range of renowned global radiology facilities can be done. In other words, we go the extra mile to seek out the very best services for our clients without impacting the cost of treatment.</p>
+                        </div>
+                        <div class="clearfix"></div>
+                            <p>Our x-ray and imaging facility features: <br>
+                            •   MRI machine 1.5Tesla <br>
+                            •   CT scan <br>
+                            •   Complete digital X-Ray <br>
+                            •   Ultrasound <br>
+                            </p>
+
+                        </div>
+                        <div>
+                        <div class="col-md-3 no_padding">
+                            <img src="img/gallery/4.jpg" class="img-responsive">
+                        </div>
+                        <div class="col-md-9 let_ip">
+                            <h1>MRI</h1>
+                            <p>We have state of the art 1.5T MRI which engineered on a foundation of technology, clinical applications and refined work flow capabilities helps to deriver excellent clinical performance. It helps Maximize productivity and improve patient care through its supreme image quality because of highly homogeneous Magnet, dedicated coils to get better signal intensity, more coverage and depth of penetration. We offer MRI Scanning with or without contrast from Head to Toe which includes</p>
+                        </div>
+                        <div class="clearfix"></div>
+                        <div class="col-md-3">
+                            <p>•    Brain<br>
+                            •   C- Spine, T- spine, LS- spine, Whole spine<br>
+                            •   Neck<br>
+                            •   Chest<br>
+                            •   Abdomen<br>
+                            •   Pelvis<br>
+                            •   Upper extremity<br>
+                            </p>
+                        </div>
+                        <div class="col-md-5">
+                            <p>
+                            •   Lower extremity<br>
+                            •   MRCP<br>
+                            •   MRA<br>
+                            •   MRV<br>
+                            •   Diffusion<br>
+                            •   Perfusion<br>
+                            •   Spectroscopy<br>
+                            </p>
+                        </div>
+
+                        </div>
+                        <div>
+                        <div class="col-md-3 no_padding">
+                            <img src="img/gallery/1-1024x683.jpg" class="img-responsive">
+                        </div>
+                        <div class="col-md-9 let_ip">
+                            <h1>CT SCAN</h1>
+                            <p>Our multi slice CT scan is designed to deliver excellent image quality and advanced dose optimization for maximum diagnostic versatility and focus patient care.  We are strong advocates for patient safety and minimal radiation exposure. Our CT with the help of a unique feature allows for minimal radiation dose delivery while maintaining high image quality.<br>
+                            •   Automatically adjust mA along X-Y-Z axis and maintain image quality what you desire<br>
+                            •   Gantry design improve X-Ray utilization and performance of X-Ray Tube and Generator
+                            We offer CT Scanning with or without contrast from Head to Toe with multi planer volume reconstruction, 3D Surface, 3D MIP and 3D Volume Rendering which includes
+                            </p>
+                        </div>
+                        <div class="clearfix"></div>
+                        <div class="col-md-3">
+                            <p> •    Head<br>
+                                •   Neck<br>
+                                •   Chest<br>
+                                •   Abdomen<br>
+                                •   Pelvis
+
+                            </p>
+                        </div>
+                        <div class="col-md-5">
+                            <p>
+                            •   Upper extremity<br>
+                            •   Lower extremity<br>
+                            •   Dental Scan<br>
+                            •   CT Scan Needle guided Biopsy
+
+                            </p>
+                        </div>
+
+                        </div>
+                        <div>
+                        <div class="col-md-3 no_padding">
+                            <img src="img/gallery/d10-1024x616.jpg" class="img-responsive">
+                        </div>
+                        <div class="col-md-9 let_ip">
+                            <h1>X-RAY</h1>
+                            <p>The X-RAY machine with its high image quality comes from the Flat-panel detector, proven to provide reliable image quality at low dose. It delivers the functionality of a compact, easy-to-use, ergonomic package while maintaining high image quality. With its straightforward design it streamlines operations so the focus is more on the patient.
+                            </p>
+                        </div>
+                        <div class="clearfix"></div>
+                        <div class="col-md-3">
+                            <p> •   Eye <br>
+                            •   Jaw<br>
+                            •   Facial bone , neck and skull<br>
+                            •   Middle ear , nasal, and sinus<br>
+                            •   Breastbone<br>
+                            •   Spine<br>
+                            •   Pelvis and hips<br>
+                            </p>
+                        </div>
+                        <div class="col-md-5">
+                            <p>
+                            •   Joints<br>
+                            •   Shoulder<br>
+                            •   Humerus<br>
+                            •   Elbow, forearm, wrist and hand<br>
+                            •   Thigh, knees , lower leg, ankle, foot, heel and toes<br>
+                            •   Abdomen and upper GI tract<br>
+                            •   Urinary tract<br>
+                            </p>
+                        </div>
+
+                        </div>
+                        <div>
+                        <div class="col-md-3 no_padding">
+                            <img src="img/gallery/d10-1024x616.jpg" class="img-responsive">
+                        </div>
+                        <div class="col-md-9 let_ip">
+                            <h1>Picture Archiving and Communications System: PACS</h1>
+                            <p>
+                            •    It’s a medical imaging technology which improves communications with referring physicians and speed time to treatment.<br>
+                            •   It provides economical storage of and convenient access to images from multiple modalities (source machine type).<br>
+                            •   Web enabled with ability to perform Teleradiology including reporting.
+
+                            </p>
+                        </div>
+
+                        </div>
+                        <div>
+                        <div class="col-md-3 no_padding">
+                            <img src="img/gallery/d7-1024x683.jpg" class="img-responsive">
+                        </div>
+                        <div class="col-md-9 let_ip">
+                            <h1>ULTRASOUND</h1>                            
+                            <div class="col-md-4">
+                                <p> •   Soft tissue of head and neck<br>
+                                •   Chest include mediastinum<br>
+                                •   Breast (unilateral or bilateral)<br>
+                                •   Abdomen<br>
+                                •   Transplanted kidney<br>
+                                •   Pregnant uterus<br>
+                                •   Transvaginal<br>
+                                •   Scrotum and contents<br>
+                                •   Infant
+                                </p>
+                            </div>
+                        </div>
+                        <div class="clearfix"></div>
+                        </div>
+                    </section>
+
+
+
                     </div>
-                    <div class="col-md-9 let_ip">
-                        <p>Medicorp Gulf Medical Clinic is an emerging diagnostic laboratory providing a full range of laboratory services in the UAE. The facility was established in 2013 in association with various partners in Dubai. The laboratory provides quality, accuracy, reliability and innovation with the purpose of improving patient care in the country. Our laboratory operates with the latest state-of-the-art consolidated solutions and innovations available in the Middle East.</p>
-                    </div>
-                    <div class="clearfix"></div>
-                        <h1>Innovation & Quality</h1>
-                        <p>At Medicorp Gulf Medical Clinic the quality of our services is at the core of the laboratory. From collection of samples, testing and result distribution, each aspect of the workflow is handled with the utmost care and attention with our highly standardized analyzers and equipment. Facilitated by the cutting-edge technology used in our laboratory, we can guarantee efficiency, accuracy, and timely results.</p>
-                    </div>
-                <div role="tabpanel" class="tab-pane" id="profile">
+                    <div role="tabpanel" class="tab-pane" id="profile">
 
                     <div class="col-md-3 no_padding">
                         <img src="img/random/diagnostic1-01.png" class="img-responsive">
@@ -108,38 +258,38 @@
                 <img src="img/icon/nd_ic1-01.svg">
                 <h3>ENT</h3>
                 <p>Diagnosis &amp; management of E.N.T diseases and postoperative care E.N.T. emergencies, as well as routine examinations Treatment of various E.N.T...</p>
-                <a href="service_1.php" class="read_more_home_serv2">Read More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                <a href="services/service_1.php" class="read_more_home_serv2">Read More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
             </div>
             <div class="col-md-4">
                 <img src="img/icon/nd_ic2-01.svg">
                 <h3>GYN/OBS</h3>
                 <p>Complete routing gynaecology, as well as obstetrics, Complete woman health care check-ups, including professional, impartial, advicem,Excellent...</p>
-                <a href="service_2.php" class="read_more_home_serv2">Read More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                <a href="services/service_2.php" class="read_more_home_serv2">Read More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
             </div>
             <div class="col-md-4">
                 <img src="img/icon/nd_ic3-01.svg">
                 <h3>Routine Dental Checkup</h3>
                 <p>Routine dental check-up, Consultations, Partial/full dentures,  Dental hygiene/de-scaling (ultrasonic), Implantology...</p>
-                <a href="service_3.php" class="read_more_home_serv2">Read More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                <a href="services/service_3.php" class="read_more_home_serv2">Read More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
             </div>
             <div class="clearfix"></div>
             <div class="col-md-4">
                 <img src="img/icon/nd_ic4-01.svg">
                 <h3>FAMILY MEDICINE</h3>
                 <p>Care and regular follow up for patients with chronic diseases such as hypertension, diabetes and asthma.Managing total family wellbeing, including...</p>
-                    <a href="service_5.php" class="read_more_home_serv2">Read More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                    <a href="services/service_5.php" class="read_more_home_serv2">Read More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
             </div>
             <div class="col-md-4">
                 <img src="img/icon/nd_ic5-01.svg">
                 <h3>OTHER CLINICS</h3>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the...</p>
-                <a href="service_6.php" class="read_more_home_serv2">Read More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                <a href="services/service_6.php" class="read_more_home_serv2">Read More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
             </div>
             <div class="col-md-4">
                 <img src="img/icon/nd_ic6-01.svg">
                 <h3>Pediatric </h3>
                 <p>Complete care for well and sick babies, Examinations and follow up, Vaccinations,Neonatal care, Neonatal follow-up and care at hospitals, Routine...</p>
-                <a href="service_4.php" class="read_more_home_serv2">Read More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                <a href="services/service_4.php" class="read_more_home_serv2">Read More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
             </div>
         </div>
     </div>
@@ -154,3 +304,18 @@
 <!-- jQuery -->
 
     <script type="text/javascript" src="build/scripts.js"></script>
+    <script type="text/javascript">
+        $('.multiple-items').slick({
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          autoplay: true,
+          autoplaySpeed: 4000,
+          dots: true,
+          // fade: true,
+        });
+    </script>
+
+    </body>
+    </html>
